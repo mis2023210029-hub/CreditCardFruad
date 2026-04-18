3af6ec217550ce4b380b10f1fb24875885cb436e
Roeun Oudom <mis2023210029@ppiu.edu.kh>
2026-04-18 10:56:42 +0700

    added python file

--- dom.py
+++ dom.py
@@ -1,1 +1,9 @@
-print("Roeun oudom!")
+def information():
+    # 1. Ask for the input first
+    name_input = input("Please input your name: ")
+    
+    # 2. Now you can print it because the variable exists
+    print(f"Stored name: {name_input}")
+
+# Call the function
+information()
